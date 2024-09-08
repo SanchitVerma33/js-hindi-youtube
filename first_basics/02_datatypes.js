719681c6c606c4d1d343d431e730d89e1da2f6cd
SanchitVerma33 <sanchitv373@gmail.com>
2024-09-08 09:25:30 +0000

    Datatypes

---
+++ first_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+ "use strict"; // treat all js code as newer version or newer js 
+
+// alert( 5 + 2 ) // we are using nodejs, not browser
+
+console.log( 3 
+    + 
+     3 )// code readability should be high
+
+console.log("Sanchit")
+
+let name = "Sanchit"
+let age = 20
+let isLoggedIn = false 
+let state;
+
+// number => 2 to the power of 53 
+// bigint 
+// String => ""
+// boolean => true/false
+// null => standalone value // representation of null value   
+// undefined =>
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
